72be16531fae4b456377e8157e19ddbdfc7722a0
lukos123 <max.kabyshev@gmail.com>
2024-09-14 11:09:48 +0200

    first commit

---
+++ _next/static/chunks/app/auth/page-81c17d50b9535b57.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[235],{4615:function(n,e,t){Promise.resolve().then(t.bind(t,1403))},1403:function(n,e,t){"use strict";t.r(e);var u=t(7437),s=t(519),c=t.n(s);e.default=n=>{let{}=n;return(0,u.jsx)("div",{className:c().auth})}},519:function(){}},function(n){n.O(0,[424,971,23,744],function(){return n(n.s=4615)}),_N_E=n.O()}]);
